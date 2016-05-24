bcfccdd8132cb03cc2b2e2eef5f8b0852ddc1114
John Wells <madmodmail@gmail.com>
2016-05-24 14:30:51 -0600

    Replace deprecated querystring functions
    `querystring.escape` and `querystring.unescape` are deprecated in favor of `querystring.stringify` and `querystring.parse`.

--- aws4.js
+++ aws4.js
@@ -219,8 +219,8 @@ RequestSigner.prototype.canonicalString = function() {
       if (normalizePath && piece === '..') {
         path.pop()
       } else if (!normalizePath || piece !== '.') {
-        if (decodePath) piece = querystring.unescape(piece)
-        path.push(encodeRfc3986(querystring.escape(piece)))
+        if (decodePath) piece = querystring.parse(piece)
+        path.push(encodeRfc3986(querystring.stringify(piece)))
       }
       return path
     }, []).join('/')
@@ -289,7 +289,7 @@ RequestSigner.prototype.parsePath = function() {
   // So if there are non-reserved chars (and it's not already all % encoded), just encode them all
   if (/[^0-9A-Za-z!'()*\-._~%/]/.test(path)) {
     path = path.split('/').map(function(piece) {
-      return querystring.escape(querystring.unescape(piece))
+      return querystring.stringify(querystring.parse(piece))
     }).join('/')
   }
 
